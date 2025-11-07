2e9ebf446ec0a6022144eed10774377ce97ad025
Robert Bornemann <robertbornemann@Roberts-MBP-2.fritz.box>
2025-11-07 18:17:24 +0100

    fixed flake8

--- src/spectorr_pipeline/etl.py
+++ src/spectorr_pipeline/etl.py
@@ -1,15 +1,19 @@
 from typing import Iterable
+
 import pandas as pd
+
 
 def extract(sources: Iterable[str]) -> pd.DataFrame:
     # TODO: read raw analyst PDFs/emails pre-parsed from S3
     return pd.DataFrame({"asset_id": [], "text": [], "source_date": []})
+
 
 def transform(df: pd.DataFrame) -> pd.DataFrame:
     # TODO: clean, tokenize, tag by asset, compute sentiment
     df = df.copy()
     df["sentiment_score"] = pd.Series(dtype="float")
     return df
+
 
 def load(df: pd.DataFrame) -> None:
     # TODO: write to curated store (e.g., S3 parquet / AWS Glue table)
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -1,5 +1,7 @@
 import pandas as pd
+
 from spectorr_pipeline.etl import transform
+
 
 def test_transform_schema():
     df = pd.DataFrame({"asset_id": ["A"], "text": ["Hello"], "source_date": ["2025-01-01"]})
